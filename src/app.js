6c4fd5c9b88b7c12c02321ee12961a71b347fa69
Nicholas <mullally.nt@gmail.com>
2022-06-18 10:44:58 -0700

    done with assignment

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "index.js";
+
+function MyApp() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>
+            <p>Loading...</p>
+        </div>;
+    } 
+}
+
+
